3447a29769a252911071cb562e8e755266939f47
qdb048sta <2169051w@gmail.com>
2023-06-13 23:43:36 +0800

    give it a try

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const client = new Client({ intents: 2048 });
 var T = new Twit({
    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-   access_token_key:         process.env.TWITTER_ACCESS_TOKEN,
-   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+   //access_token_key:         process.env.TWITTER_ACCESS_TOKEN,
+   //access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
   bearer_token:  process.env.BEARER_TOKEN
